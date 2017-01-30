c555dd57691e52ed7445193e127d98b83e10e4ca
Flavia Nagwekar <nunesflavia7@gmail.com>
2017-01-30 17:30:44 -0600

    feat(module-api):add authorization middleware

--- server/middlewares/validateToken.js
+++ server/middlewares/validateToken.js
@@ -1,6 +1,8 @@
 var jwt = require('jsonwebtoken');
 var database_config = require('../config');
 var MovieFlixError = require('../controllers/ErrorController.js');
+var User = require('../models/user.server.model.js');
+var mongoose = require('mongoose');
 
 //var router = require('../routes/routes.js');
 
@@ -10,28 +12,42 @@ var MovieFlixError = require('../controllers/ErrorController.js');
 
 //route middleware to identify a token
 
-module.exports = function(req,res,next){
-    console.log("Inside validate token");
-    //token can be present in any of the url parameters, headers or body
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+var AuthMiddleware ={
+    validateToken : function(req,res,next){
+        //token can be present in any of the url parameters, headers or body
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+        //decoding token
+        if(token){
+            jwt.verify(token,database_config.secret,function(err,decoded){
+                if(err){
+                    return res.status(401).json(new MovieFlixError('Authentication failed.','').sendErrMessage());
+                }
+                else{
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        }
+        else{
+            return res.status(401).send(new MovieFlixError('Not able to authenticate user.','').sendErrMessage());
+        }
+    },
     
-    //decoding token
-    console.log(token);
-    if(token){
-        jwt.verify(token,database_config.secret,function(err,decoded){
-            console.log(err);
-            if(err){
-                return res.status(401).json(new MovieFlixError('Authentication failed.','').sendErrMessage());
-            }
-            else{
-                console.log("In else");
-                req.decoded = decoded;
-                console.log(decoded)
-                next();
-            }
-        });
+    authorizeUser : function(req,res,next){
+        User.findById(mongoose.Types.ObjectId(req.body.userid)).then(
+            function(user){
+                if(user.roles.indexOf('admin') != -1)
+                {
+                    next();
+                }
+                else{
+                    return res.status(403).send(new MovieFlixError('You are not authorized to perform this operation.','').sendErrMessage());
+                }
+            },function(err){
+                return res.status(401).send(new MovieFlixError('Not able to authenticate user.','').sendErrMessage());
+            });
     }
-    else{
-        return res.status(401).send(new MovieFlixError('Not able to authenticate user.','').sendErrMessage());
-    }
-};
+    
+}
+module.exports = AuthMiddleware;
