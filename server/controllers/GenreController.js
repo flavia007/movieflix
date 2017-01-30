21e388e6587a37609b5e5b544f19e8ea60f8cd69
Flavia Nagwekar <nunesflavia7@gmail.com>
2017-01-30 17:32:45 -0600

    feat(module-api):add Genre Controller for managing genres

--- server/controllers/GenreController.js
+++ server/controllers/GenreController.js
@@ -4,7 +4,7 @@ var Genre = require('../models/genre.server.model.js');
 var GenreCtrl = {
     
     //Returns promise object when adding new genres
-    addGenre : function(genreItem){
+    addUpdateGenre : function(genreItem){
          return Genre.findOneAndUpdate(
                 { genreType: genreItem },
                 {$set: { genreType: genreItem }} ,
